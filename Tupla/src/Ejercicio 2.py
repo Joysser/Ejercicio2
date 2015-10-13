ef41606240e4614aaf0416bf1b20d7d7afff9ffe
Alumno <Alumno@LABSI-13>
2015-10-13 10:37:35 -0500

    El ejercicio 2

---
+++ Tupla/src/Ejercicio 2.py
@@ -0,0 +1,18 @@
+#Programa que muestre un menu
+
+print 'Si desea entrar al menu de ARCHIVO ingresar la opcion 1'
+print 'Si desea entrar al menu de BUSQUEDA ingresar la opcion 2'
+print 'Si desea entrar al menu de SALIR ingresar la opcion 3'
+a=input('Introduzca algunas de las opciones mencionadas=')
+if a == 1 :
+    print 'Bienvenido al menu de ARCHIVOS'
+else:
+    if a==2:
+        print 'Bienvenido al menu de BUSQUEDA'
+    else:
+        if a==3:
+            print 'Bienvenido al menu de SALIDA'
+        else:
+            print 'Ingrese una de las opciones validas mencionadas anteriormente'
+
+    
